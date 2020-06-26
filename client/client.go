8ebeb35a3dc175767e9c865f68656b1e41a3acfd
Matt Selph <matt.selph@gmail.com>
2020-06-26 11:13:31 -0400

    creates a new client successfully

--- client/client.go
+++ client/client.go
@@ -1,7 +1,9 @@
 package client
 
 import (
+	"fmt"
 	"net/http"
+	"net/url"
 	"time"
 )
 
@@ -20,12 +22,59 @@ type WOTClient struct {
 }
 
 // NewClient returns a pointer to a new client object
-func NewClient(applicationID string) *WOTClient {
+func NewClient(applicationID string, realm string) *WOTClient {
 	return &WOTClient{
 		client: &http.Client{
 			Timeout: DefaultClientTimeout,
 		},
 		ApplicationID: applicationID,
-		baseURL:       BaseURL,
+		baseURL:       SetRealm(realm),
+	}
+}
+
+// SetRealm sets the API endpoint to other realms
+func SetRealm(realm string) string {
+	var url string
+
+	switch realm {
+	case "na":
+		url = "https://api.worldoftanks.com/wot/"
+	case "eu":
+		url = "https://api.worldoftanks.eu/wot/"
+	case "ru":
+		url = "https://api.worldoftanks.ru/wot/"
+	case "asia":
+		url = "https://api.worldoftanks.asia/wot/"
+	default:
+		url = "https://api.worldoftanks.com/wot/"
+	}
+
+	return url
+}
+
+// SetTimeout allows overriding of the default 5 second client timeout
+func (w *WOTClient) SetTimeout(d time.Duration) {
+	w.client.Timeout = d
+}
+
+// OverrideURL overrides the default URL in the client
+func (w *WOTClient) OverrideURL(url string) {
+	w.baseURL = url
+}
+
+// GetPlayer retrieves a player record
+func (w *WOTClient) GetPlayer(playerName string) {
+	endpoint := "/account/list/?"
+
+	url := url.Values{}
+	url.Add("application_id", w.ApplicationID)
+	url.Add("search", playerName)
+	endpoint = w.baseURL + endpoint + url.Encode()
+
+	request, _ := http.NewRequest(http.MethodPost, endpoint, nil)
+	_, err := w.client.Do(request)
+
+	if err != nil {
+		fmt.Println(err)
 	}
 }
--- client/client_test.go
+++ client/client_test.go
@@ -2,15 +2,23 @@ package client
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"testing"
+	"time"
 )
 
-func TestClient(t *testing.T) {
+const (
+	appID      = "dummy"
+	realm      = "na"
+	playerName = "dummy"
+)
+
+func TestClientSetup(t *testing.T) {
 	t.Run("Returns a pointer to a new client object", func(t *testing.T) {
 		t.Helper()
 
-		got := NewClient("dummy")
+		got := NewClient("dummy", "na")
 		want := &WOTClient{
 			client: &http.Client{
 				Timeout: DefaultClientTimeout,
@@ -22,5 +30,85 @@ func TestClient(t *testing.T) {
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("Got %+v\n want %+v\n", got, want)
 		}
+	})
+
+	t.Run("Set a different timeout than the default", func(t *testing.T) {
+		t.Helper()
+
+		client := NewClient("dummy", "na")
+		client.SetTimeout(10 * time.Second)
+
+		got := client.client.Timeout
+		want := 10 * time.Second
+
+		if got != want {
+			t.Errorf("Got %d want %d", got, want)
+		}
+	})
+
+	t.Run("Set a different realm", func(t *testing.T) {
+		t.Helper()
+
+		var realms = []struct {
+			realm string
+			want  string
+		}{
+			{"na", "https://api.worldoftanks.com/wot/"},
+			{"eu", "https://api.worldoftanks.eu/wot/"},
+			{"ru", "https://api.worldoftanks.ru/wot/"},
+			{"asia", "https://api.worldoftanks.asia/wot/"},
+		}
+
+		for _, tt := range realms {
+			t.Run(tt.realm, func(t *testing.T) {
+				client := NewClient("dummy", tt.realm)
+				got := client.baseURL
+
+				if got != tt.want {
+					t.Errorf("got %s want %s", got, tt.want)
+				}
+			})
+		}
+	})
+
+	t.Run("Override the API URL", func(t *testing.T) {
+		t.Helper()
+
+		client := NewClient("dummy", "na")
+		want := "https://example.com"
+		client.OverrideURL(want)
+		got := client.baseURL
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+}
+
+func TestAccountEndpoints(t *testing.T) {
+
+	t.Run("should POST data", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.Method != http.MethodPost {
+				t.Errorf("Expected `POST` got `%s`", r.Method)
+			}
+		}))
+
+		client := NewClient(appID, realm)
+		client.OverrideURL(ts.URL)
+		client.GetPlayer(playerName)
+	})
+
+	t.Run("should hit the correct URL", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			want := "/account/list/?application_id=dummy&search=dummy"
+			if r.URL.String() != want {
+				t.Errorf("Expected %s got %s", want, r.URL.String())
+			}
+		}))
+
+		client := NewClient(appID, realm)
+		client.OverrideURL(ts.URL)
+		client.GetPlayer(playerName)
 	})
 }
