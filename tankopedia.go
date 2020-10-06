f21334d80ca35a4cbf296e4e3c7e33a3acf0c89d
matts <matt.selph@gmail.com>
2020-10-06 13:10:56 -0400

    adding arena stuff to tankopedia

--- tankopedia.go
+++ tankopedia.go
@@ -1,5 +1,20 @@
 package gotanking
 
+package gotanking
+
+// Arena represents data from the encyclopedia/arenas endpoint
+type Arena struct {
+	Data map[string]ArenaRecord `json:"data"`
+}
+
+// ArenaRecord represents a single arena record
+type ArenaRecord struct {
+	Name string `json:"name_i18n"`
+	Camo string `json:"camouflage_type"`
+	Desc string `json:"description"`
+	ID   string `json:"arena_id"`
+}
+
 /*
 
 // ListMaps queries the encyclopedia/arenas endpoint
