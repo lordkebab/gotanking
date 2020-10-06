1a1a8a6c0b665c630ca6954aa654dd72fcb0e5f5
matts <matt.selph@gmail.com>
2020-10-06 13:01:40 -0400

    flattened repo structure

---
+++ arena.go
@@ -0,0 +1,14 @@
+package model
+
+// Arena represents data from the encyclopedia/arenas endpoint
+type Arena struct {
+	Data map[string]ArenaRecord `json:"data"`
+}
+
+// ArenaRecord represents a single arena record
+type ArenaRecord struct {
+	Name string `json:"name_i18n"`
+	Camo string `json:"camouflage_type"`
+	Desc string `json:"description"`
+	ID   string `json:"arena_id"`
+}
