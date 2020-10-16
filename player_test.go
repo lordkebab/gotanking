6db73295e927a062bacb6571a5e281646ed29340
matts <matt.selph@gmail.com>
2020-10-16 15:53:40 -0400

    added test for getting a player account_id

---
+++ player_test.go
@@ -0,0 +1,30 @@
+package gotanking
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestGetAccount(t *testing.T) {
+	testServer := ServerSetup()
+	defer testServer()
+
+	client, _ := NewClient("dummy", SetBaseURL(server.URL))
+	mux.HandleFunc("/account/list", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, Fixture("account/list.json"))
+	})
+
+	search := "lulz_man"
+
+	playerRec, err := client.GetAccount(search, nil)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Println(playerRec)
+}
